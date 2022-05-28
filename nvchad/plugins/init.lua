1b86deabf26c74b6ca61a51fdd5f780379ee13f1
Ben Cooper <mnib.mnib@gmail.com>
2022-05-28 13:09:21 +0300

    nvchad dotfiles

---
+++ nvchad/plugins/init.lua
@@ -0,0 +1,25 @@
+return {
+  user = {
+    ["goolord/alpha-nvim"] = {
+      disable = false,
+    },
+    ["tpope/vim-unimpaired"] = {},
+    ["tpope/vim-repeat"] = {},
+    ["tpope/vim-surround"] = {},
+    ["karb94/neoscroll.nvim"] = {
+      config = function()
+        require("neoscroll").setup()
+      end,
+
+      -- lazy loading
+      setup = function()
+        nvchad.packer_lazy_load "neoscroll.nvim"
+      end,
+    },
+    ["luukvbaal/stabilize.nvim"] = {
+      config = function()
+        require("stabilize").setup()
+      end
+    },
+  },
+}
