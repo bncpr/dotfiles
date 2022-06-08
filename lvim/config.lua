d7f30ea260b9d98dadc4ff5520638ae87e8ebfd2
Ben Cooper <bcooper@drivenets.com>
2022-06-08 21:35:38 +0300

    flake8 as linter

--- lvim/config.lua
+++ lvim/config.lua
@@ -159,9 +159,9 @@ formatters.setup {
 }
 
 -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
-  -- { command = "yapf", filetypes = { "python" } },
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { command = "flake8", filetypes = { "python" } },
   -- {
   --   -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
   --   command = "shellcheck",
@@ -174,7 +174,7 @@ formatters.setup {
     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
     -- filetypes = { "javascript", "python" },
   -- },
--- }
+}
 
 -- Additional Plugins
 lvim.plugins = {
