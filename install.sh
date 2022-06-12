38df4c817d4fd8ea38e983f867f3ea4addb692a7
Ben Cooper <mnib.mnib@gmail.com>
2022-06-13 00:49:19 +0300

    lvim ftplugin clangd fix

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -sf zshrc ~/.zshrc
+ln -sf aliases ~/.aliases
---
+++ lvim/ftplugin/c.lua
@@ -0,0 +1,4 @@
+vim.cmd([[
+  setlocal ts=4 sts=4 sw=4 noexpandtab
+  noremap <buffer> <localleader>; mmA;<esc>`m
+]])
