2b42383f7e47f907c85f217c87aa6bacf2068e47
Ben Cooper <mnib.mnib@gmail.com>
2022-06-26 22:47:59 +0300

    lvim comment

--- lvim/config.lua
+++ lvim/config.lua
@@ -85,6 +85,7 @@ lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 -- lvim.builtin.nvimtree.show_icons.git = 1
+lvim.builtin.comment.mappings.extra = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.active = false
