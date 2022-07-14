85ddccb615eec0e119ae0961045ad4af0fda2bdb
Ben Cooper <mnib.mnib@gmail.com>
2022-07-14 23:35:52 +0300

    lvim

--- lvim/ftplugin/c.lua
+++ lvim/ftplugin/c.lua
@@ -1,7 +1,6 @@
 vim.cmd([[
   setlocal ts=4 sts=4 sw=4 noexpandtab
-  set colorcolumn=88
-  highlight ColorColumn ctermbg=darkgray
   noremap <buffer> <localleader>; mmA;<esc>`m
-  imap <buffer> ,j <esc>o{<enter>
+  noremap <buffer> d][ V][d
+  imap <buffer> <localleader>j <esc>o{<enter>
 ]])
