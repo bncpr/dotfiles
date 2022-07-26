66b821bcc43528fddc70dce66807609f6f8aaabc
Ben Cooper <mnib.mnib@gmail.com>
2022-07-27 00:05:53 +0300

    lvim

--- lvim/ftplugin/c.lua
+++ lvim/ftplugin/c.lua
@@ -1,6 +1,6 @@
 vim.cmd([[
   setlocal ts=4 sts=4 sw=4 noexpandtab
   noremap <buffer> <localleader>; mmA;<esc>`m
-  noremap <buffer> d][ V][d
   imap <buffer> <localleader>j <esc>o{<enter>
+  let @t="dwea_t"
 ]])
