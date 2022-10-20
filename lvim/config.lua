75a8b99d03c1e495ebbe7a4517c892d11e59b8f4
Ben Cooper <mnib.mnib@gmail.com>
2022-10-20 22:21:03 +0300

    lvim catppuccin

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,7 +1,7 @@
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = false
-lvim.colorscheme = "tokyonight"
+lvim.colorscheme = "catppuccin"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -122,6 +122,7 @@ lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "startify"
 lvim.builtin.notify.active = false
 lvim.builtin.nvimtree.setup.view.width = 40
+lvim.builtin.lualine.options.theme = "catppuccin"
 -- lvim.builtin.comment.mappings.extra = true
 
 lvim.builtin.terminal.active = true
@@ -434,11 +435,6 @@ lvim.plugins = {
     event = "BufRead"
   },
   {
-    "heavenshell/vim-pydocstring",
-    run = "make install",
-    ft = "python"
-  },
-  {
     "andymass/vim-matchup",
     event = "CursorMoved",
     config = function()
@@ -492,6 +488,64 @@ lvim.plugins = {
   },
   {
     "nathanalderson/yang.vim"
+  },
+  {
+    "catppuccin/nvim",
+    as = "catppuccin",
+    config = function()
+      vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+      require("catppuccin").setup({
+        integrations = {
+          cmp = true,
+          dashboard = true,
+          gitsigns = true,
+          harpoon = true,
+          leap = true,
+          lsp_trouble = true,
+          fidget = true,
+          notify = true,
+          nvimtree = true,
+          symbols_outline = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+          ts_rainbow = true,
+          which_key = true,
+
+          -- Special integrations, see https://github.com/catppuccin/nvim#special-integrations
+          indent_blankline = {
+            enabled = true,
+            colored_indent_levels = false,
+          },
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+            },
+          },
+        },
+      })
+      vim.api.nvim_command "colorscheme catppuccin"
+    end
+  },
+  {
+    'j-hui/fidget.nvim',
+    config = function()
+      require("fidget").setup({
+        window = {
+          blend = 0,
+        }
+      })
+    end
   }
 }
 
