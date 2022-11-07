2d40a716a8782387378702ffa330a6c37be204bc
Ben Cooper <mnib.mnib@gmail.com>
2022-11-07 21:08:52 +0000

    lvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -462,21 +462,13 @@ lvim.plugins = {
 			require("osc52").setup({
 				silent = true,
 			})
-			-- OSC52
-			local function copy(lines, _)
-				require("osc52").copy(table.concat(lines, "\n"))
+			local function copy()
+				if vim.v.event.operator == "y" then
+					require("osc52").copy_register(vim.v.event.regname)
+				end
 			end
 
-			local function paste()
-				return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
-			end
-
-			-- Now the '+' register will copy to system clipboard using OSC52
-			vim.g.clipboard = {
-				name = "osc52",
-				copy = { ["+"] = copy, ["*"] = copy },
-				paste = { ["+"] = paste, ["*"] = paste },
-			}
+			vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
 		end,
 	},
 	{
