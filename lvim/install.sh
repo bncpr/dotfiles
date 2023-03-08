e3a1fdf66c29196642bda9ae7cf96c64c963dfae
Ben Cooper <mnib.mnib@gmail.com>
2023-03-08 20:24:55 +0000

    install.sh

--- lvim/install.sh
+++ lvim/install.sh
@@ -2,9 +2,9 @@
 
 ln -srf config.lua ~/.config/lvim/config.lua
 
-mkdir -pv ~/.config/lvim/ftplugin
-ln -srf ftplugin/c.lua ~/.config/lvim/ftplugin/c.lua
-ln -srf ftplugin/c.lua ~/.config/lvim/ftplugin/cpp.lua
+mkdir -pv ~/.config/nvim/after/ftplugin
+ln -srf ftplugin/c.lua ~/.config/nvim/after/ftplugin/c.lua
+ln -srf ftplugin/c.lua ~/.config/nvim/after/ftplugin/cpp.lua
 
 mkdir -pv ~/.config/lvim/luasnippets
 ln -srf snippets/* ~/.config/lvim/luasnippets/
