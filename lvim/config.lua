2cd44338f1ffb6c73022c8ba5cf8b6eaa1def5c9
Ben Cooper <mnib.mnib@gmail.com>
2023-03-22 19:05:23 +0000

    noice

--- lvim/config.lua
+++ lvim/config.lua
@@ -602,15 +602,15 @@ lvim.plugins = {
 			vim.api.nvim_set_keymap("n", "<c-/>", "<cmd>TmuxNavigatePrevious<cr>", opts)
 		end,
 	},
-	{
-		"vigoux/notifier.nvim",
-		event = "BufRead",
-		config = function()
-			require("notifier").setup({
-				-- You configuration here
-			})
-		end,
-	},
+	-- {
+	-- 	"vigoux/notifier.nvim",
+	-- 	event = "BufRead",
+	-- 	config = function()
+	-- 		require("notifier").setup({
+	-- 			-- You configuration here
+	-- 		})
+	-- 	end,
+	-- },
 	{
 		"RRethy/nvim-treesitter-textsubjects",
 		config = function()
@@ -792,6 +792,51 @@ lvim.plugins = {
 		lazy = true,
 	},
 	{ "nvim-treesitter/playground" },
+	{
+		"folke/noice.nvim",
+		config = function()
+			require("noice").setup({
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+				},
+				-- you can enable a preset for easier configuration
+				routes = {
+					{
+						view = "notify",
+						filter = { event = "msg_showmode" },
+					},
+					{
+						filter = {
+							event = "msg_show",
+							kind = "",
+							find = "written",
+						},
+						opts = { skip = true },
+					},
+				},
+				presets = {
+					bottom_search = false, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = false, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			})
+		end,
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
 }
 
 -- Autocommands (https://eovim.io/doc/user/autocmd.html)
