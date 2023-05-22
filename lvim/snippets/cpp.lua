b5b34bd3104da0c1bf766a8e93d49f9aad5cebea
Ben Cooper <mnib.mnib@gmail.com>
2023-05-22 20:04:15 +0000

    cpp snips

--- lvim/snippets/cpp.lua
+++ lvim/snippets/cpp.lua
@@ -1,4 +1,35 @@
 ---@diagnostic disable: undefined-global
 return {}, {
+	s(
+		"} else",
+		fmt(
+			[[
+      }} else {{
+          {1}
+      }}
+    ]],
+			i(1)
+		),
+		{ condition = conds.line_begin + conds.line_end }
+	),
 	s("return", fmt("return {1};", { i(1) }), { condition = conds.line_begin }),
+	s(
+		"if",
+		fmt(
+			[[
+      if ({1})
+      {{
+          {2}
+      }}
+    ]],
+			{ i(1), i(2) }
+		),
+		{ condition = conds.line_begin }
+	),
+	s(
+		{ trig = "(%w+):", regTrig = true },
+		f(function(_, snip)
+			return snip.trigger .. ":"
+		end, {})
+	),
 }
