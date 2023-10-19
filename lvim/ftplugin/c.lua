6f7f3c097654461aa986dc4eb00c46188baf7e4a
Ben Cooper <mnib.mnib@gmail.com>
2023-10-19 22:30:40 +0300

    remove lvim

--- lvim/ftplugin/c.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd([[
-  setlocal ts=4 sts=4 sw=4 noexpandtab
-  noremap <buffer> <localleader>; mmA;<esc>`m
-  imap <buffer> <localleader>j <esc>o{<enter>
-  map <buffer> <localleader>d $xo{<enter>
-  let @t="dwea_t"
-  noremap <buffer> <localleader>d] V][d
-  command! RemoveRC :%s/int rc = \(.*\);\n\s*if (rc)/if (\1)/e
-]])
--- lvim/snippets/all.lua
+++
@@ -1,8 +0,0 @@
----@diagnostic disable: undefined-global
-return {
-	s("qmkcomb", fmt("COMB({1}, {2}, {3})", { i(1, "name"), i(2, "result"), i(3, "combo") })),
-	s(
-		"qmksubs",
-		fmt('SUBS({1}, "{2}"{3}, {4})', { i(1, "name"), i(2, "result"), i(3, "rest of string"), i(4, "combo") })
-	),
-}
